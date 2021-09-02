ba8eca4d881a4b751c5b04adb510fe76672ba7cd
nasjones <46979749+nasjones@users.noreply.github.com>
2021-09-02 18:45:19 -0400

    parallel update

--- await/await.js
+++ await/await.js
@@ -4,11 +4,21 @@ async function processForm(evt) {
 	evt.preventDefault();
 
 	let num = $(evt.target).find("input")[0].value;
-	console.log(num);
-	for (let i = 0; i < 4; i++) {
-		let res = await axios.get(`${numberAPI}${num}/trivia?json`);
-		handleResponse(res.data);
-	}
+	
+	let req1 = await axios.get(`${numberAPI}${num}/trivia?json`);
+	let req2 = await axios.get(`${numberAPI}${num}/trivia?json`);
+	let req3 = await axios.get(`${numberAPI}${num}/trivia?json`);
+	let req4 = await axios.get(`${numberAPI}${num}/trivia?json`);
+
+	let res1 = await req1;
+	let res2 = await req2;
+	let res3 = await req3;
+	let res4 = await req4;
+
+	handleResponse(res1.data);
+	handleResponse(res2.data);
+	handleResponse(res3.data);
+	handleResponse(res4.data);
 }
 
 function handleResponse(resp) {
